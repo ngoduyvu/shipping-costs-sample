485607ff96e94b8fa15bc875a58cd74286d5bc09
ngoduyvu <ngoduyvu@gmail.com>
2018-02-23 17:31:44 +1000

    commit

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
 
-
-from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
-
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-
 import urllib
 import json
 import os
@@ -64,4 +55,4 @@ if __name__ == '__main__':
 
     print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=True, port=port, host='0.0.0.0')
